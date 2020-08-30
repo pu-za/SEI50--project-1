bc487597ba255e1039e1575374a2bf7f5e4cf8dc
puja <gc.puja1@yahoo.com>
2020-08-30 12:16:47 +0100

    picture in

--- scripts/app.js
+++ scripts/app.js
@@ -1,32 +1,30 @@
 function init() {
 
   const grid = document.querySelector('.grid')
-
+  
   //Grid//
   const cells = []
   const width = 10
   const gridCellCount = width * width
-
+  let shooterPosition = 90
 
   
   function createGrid() {
     for (let i = 0; i < gridCellCount; i++) {
       const cell = document.createElement('div')
-      cell.setAttribute('data-index', i)
+      cell.setAttribute('boxes', i)
+      
       cells.push(cell)
       grid.appendChild(cell)
+
     }
-    
+    addShooter(shooterPosition)
   }
-  function addShooter(position) {
-    cells[position].classList.add('shooter')
+  function addShooter() {
+    cells[shooterPosition].classList.add('shooter')
   }
   createGrid()
 
 }
-
-
-
-
 
 window.addEventListener('DOMContentLoaded', init)
